f0d9c9a0148cc3f1548cb6974105151792065eef
Mori-Chan <59697686+Mori-Chan@users.noreply.github.com>
2020-05-01 01:50:00 +0900

    Add files via upload

---
+++ src/test/java/practice/java/practice_java/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package practice.java.practice_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
